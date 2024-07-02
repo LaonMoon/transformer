69d65d651edb2fa42a2132f7fc9891f7eb86f3b6
LaonMoon <laonmun@google.com>
2024-07-02 06:27:27 +0000

    feat: Add Embedding layer and positional_encoding

---
+++ model.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import tokenizer
+
+text = "Hello, how are you? ich bin Max."
+tokenizer = tokenizer.tokenizer()
+input_indices = tokenizer.encode(text)
+
+input_size = 37000
+embedding_dim = 512
+
+def embedding_layer(input_indices):
+    embedding_layer = nn.Embedding(input_size, embedding_dim, padding_idx=1)
+
+    input_indices = torch.LongTensor(input_indices)
+    print(len(input_indices))
+    embedded_input = embedding_layer(input_indices)
+
+    return embedded_input
+
+def positional_encoding(seq_len, d_model):
+    pos_enc = np.zeros((seq_len, d_model))
+    for pos in range(seq_len):
+        for i in range(0, d_model, 2):
+            pos_enc[pos, i] = np.sin(pos / (10000 ** ((2 * i)/d_model)))
+            pos_enc[pos, i+1] = np.cos(pos / (10000 ** ((2 * (i+1))/d_model)))
+    return pos_enc
+
+seq_length = 128 # max_len에 따름
+embedding_dim = 512
+pos_encoding = positional_encoding(seq_length, embedding_dim)
+
+print("Positional Encoding Shape:", pos_encoding.shape)
+
+# input embedding과 positional encoding을 더함
+pos_encoding = torch.FloatTensor(pos_encoding)
+embedded_input = embedding_layer(input_indices)
+pos_encoding = pos_encoding[:embedded_input.shape[0], :]
+embedded_input_with_pos = embedded_input + pos_encoding.unsqueeze(0)
+
+print("Input text:", text)
+print("Input indices:", input_indices)
+
+print("Embedded input:", embedded_input)
+print("Shape of embedded input:", embedded_input.shape)
+
+print("Embedded input with positional encoding:", embedded_input_with_pos)
+print("Shape of embedded input with positional encoding:", embedded_input_with_pos.shape)
--- tokenizer.py
+++ tokenizer.py
@@ -3,19 +3,22 @@
 from datasets import load_dataset
 import sentencepiece as spm
 
-spm.SentencePieceTrainer.train(input="train_de_en.json", model_prefix="sentencepiece_model", vocab_size=37000)
-tokenizer = spm.SentencePieceProcessor()
-tokenizer.load("sentencepiece_model.model")
+def tokenizer():
+    spm.SentencePieceTrainer.train(input="train_de_en.json", model_prefix="sentencepiece_model", vocab_size=37000)
+    tokenizer = spm.SentencePieceProcessor()
+    tokenizer.load("sentencepiece_model.model")
 
-# test
-text = "Hello, This is test of sentencepiece. Ich komme aus Korea."
-tokens = tokenizer.encode_as_pieces(text)
-print(tokens)
+    # test
+    text = "Hello, This is test of sentencepiece. Ich komme aus Korea."
+    tokens = tokenizer.encode_as_pieces(text)
+    print(tokens)
 
-# vocab size
-vocab_size = tokenizer.get_piece_size()
-print("Vocabulary size:", vocab_size)
+    # vocab size
+    vocab_size = tokenizer.get_piece_size()
+    print("Vocabulary size:", vocab_size)
 
-print("Sample tokens:")
-for i in range(10):
-    print(tokenizer.id_to_piece(i))
+    print("Sample tokens:")
+    for i in range(10):
+        print(tokenizer.id_to_piece(i))
+
+    return tokenizer
