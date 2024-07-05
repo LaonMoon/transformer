edbe5bb89299b63f3071c1bfcac882531a5c61f7
LaonMoon <laonmun@google.com>
2024-07-05 11:11:34 +0000

    feat: Add call tokenizer class part

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 import numpy as np
-import tokenizer
+from tokenizer import Tokenizer
 
 class Embedding:
-    def __init__(self):
+    def __init__(self, input_indices):
         self.input_size = 37000
         self.seq_length = 128 # max_len에 따름
         self.embedding_dim = 512
@@ -24,7 +24,7 @@ class Embedding:
                 pos_enc[pos, i+1] = np.cos(pos / (10000 ** ((2 * (i+1))/d_model)))
         return pos_enc
     
-    def forward(self,):
+    def forward(self):
         pos_encoding = self.positional_encoding(self.seq_length, self.embedding_dim)
 
         # print("Positional Encoding Shape:", pos_encoding.shape)
@@ -46,11 +46,24 @@ class Embedding:
         
         return embedded_input_with_pos
 
-text = "Hello, how are you? ich bin Max."
-tokenizer = tokenizer.tokenizer()
-input_indices = tokenizer.encode(text)
+# tokenizer = Tokenizer(train_path="train_de_en.json")
+# tokenizer.train()
 
-embedding = Embedding()
+tokenizer_instance = Tokenizer(train_path="train_de_en.json", model_path="sentencepiece.model")
+text = "Hello, This is a test of sentencepiece. Ich komme aus Korea. Schön, dich kennenzulernen."
+tokenized_samples = tokenizer_instance.tokenize(text, num_samples=3)
+
+for i, sample in enumerate(tokenized_samples, 1):
+    print(f"Sample {i}: {sample}")
+
+print("Vocabulary size:", tokenizer_instance.get_vocab_size())
+encoded_pieces = tokenizer_instance.encode_as_pieces(text)
+print("Encoded as pieces:", encoded_pieces)
+
+# 인코딩된 텍스트를 임베딩에 전달
+# 여기서는 첫 번째 샘플을 사용
+input_indices = [tokenizer_instance.tokenizer.piece_to_id(piece) for piece in tokenized_samples[0]]
+embedding = Embedding(input_indices)
 embedded_input_with_pos = embedding.forward()
 
 print("Embedded input with positional encoding:")
